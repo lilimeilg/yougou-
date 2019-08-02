284c7f6b61631a61dce7b0be08aaf170f661263d
lilimei <369667724@qq.com>
2019-08-02 12:57:11 +0800

    商品列表页面的结构和动态渲染

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,4 +1,5 @@
 // pages/goods_list/index.js
+import {request} from "../../request/index"
 Page({
 
   /**
@@ -9,14 +10,25 @@ Page({
      {id:0,title:"综合",isActive:true},
      {id:1,title:"销量",isActive:false},
      {id:2,title:"价格",isActive:false},
-   ]
+   ],
+   goodsList:[]
   },
-
+  // 全局接口参数
+ QueryParams:{
+   query:"",//搜索关键字
+   cid:"",//商品分类页面传递过来的分类id
+   pagenum:1,//当前页码
+   pagesize:10//每页显示条数
+ },
   /**
    * 生命周期函数--监听页面加载
    */
+  // onLoad 页面开始加载的时候触发  形参中可以获取到页面的url参数
+  // 小程序页面加载的时候，可以通过options获取到上一个页面标签的属性以及属性值
   onLoad: function (options) {
     console.log(options);
+    this.QueryParams.cid=options.cid
+    this.getGoodsList()
   },
 // 改变tabs标签的选中效果
 handleTitleChange(e){
@@ -31,6 +43,14 @@ handleTitleChange(e){
   });
   // 修改了data需要重新赋值
   this.setData({tabs})
+},
+getGoodsList(){
+  request({url:"/goods/search",data:this.QueryParams}).then(res=>{
+    console.log(res);
+    this.setData({
+      goodsList:res.goods
+    })
+  })
 }
   
 })
